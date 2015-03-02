1af0c6e38fbb86fa50f9fc23e6c0dfdab588c320
rohx89 <rohx89@192.168.0.12>
2015-03-02 09:46:30 +0100

    Initial Commit

---
+++ VKRun/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VKRun
+//
+//  Created by vico kuras on 02.03.15.
+//  Copyright (c) 2015 vico kuras. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
